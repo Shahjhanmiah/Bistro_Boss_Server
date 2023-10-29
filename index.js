45ceca521a623a0553a89abb0657177181949239
mdshahjhan <mdshahjha478877@gmail.com>
2023-10-29 11:52:12 +0600

    menu

--- index.js
+++ index.js
@@ -156,6 +156,13 @@ async function run() {
       res.send(result)
 
     })
+    // menu delete api 
+    app.delete('/menu/:id', verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // review related apis
     app.get('/reviews', async (req, res) => {
